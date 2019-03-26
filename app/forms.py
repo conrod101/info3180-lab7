3430f5d61df222b83e682a1d5f9a49f74ea3b797
conrod <conrod.smith36@gmail.com>
2019-03-26 21:45:44 +0000

    UploadForm that has two fields. A TextArea field called description that requires a user to fill in some data and a FileField called photo that only allows images to be uploaded.

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,13 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms.validators import DataRequired
+from wtforms import TextField, TextAreaField 
 
+
+
+class UploadForm(FlaskForm):
+    description = TextAreaField('Description', validators=[DataRequired('A description is required!')])
+    photo = FileField('Photo', validators=[
+    FileRequired(),FileAllowed(['jpg', 'png','jpeg','gif', 'Images only!'])
+    ])
+    
